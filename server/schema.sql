06167bd109b30d5b57e818de739ffe20323877a2
Hack Reactor Students <accounts@hackreactor.com>
2015-07-24 18:41:33 -0700

    got our tables into the database

--- server/schema.sql
+++ server/schema.sql
@@ -3,11 +3,23 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  userid int(6),
+  message varchar(160),
+  roomid int(6)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Describe your table here.*/
+  userid int(6),
+  username varchar(30)
+);
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  roomid int(6),
+  roomname varchar(30)
+);
 
 
 
